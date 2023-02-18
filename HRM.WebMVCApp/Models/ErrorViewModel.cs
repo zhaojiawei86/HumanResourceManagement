f847254728ce2a76f11207bd4c472f6d71962685
zhaojiawei86 <59852184+zhaojiawei86@users.noreply.github.com>
2023-02-18 14:12:48 -0500

    create projects for solution

---
+++ HRM.WebMVCApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HRM.WebMVCApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
